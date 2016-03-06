cc435b646cace868e7c52c298883ccbc25577803
srojak <srojak@srojak.com>
2016-03-06 16:06:08 -0600

    Rewrite the load function

--- load.r
+++ load.r
@@ -1,18 +1,8 @@
-pwrcons <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", colClasses = rep("character", times=9))
-# convert columns
-pwrcons$Date <- as.Date(pwrcons$Date, format="%d/%m/%Y")
-pwrcons$Global_active_power <- as.double(pwrcons$Global_active_power)
-pwrcons$Global_reactive_power <- as.double(pwrcons$Global_reactive_power)
-pwrcons$Voltage <= as.double(pwrcons$Voltage)
-pwrcons$Global_intensity <- as.double(pwrcons$Global_intensity)
-pwrcons$Sub_metering_1 <- as.double(pwrcons$Sub_metering_1)
-pwrcons$Sub_metering_2 <- as.double(pwrcons$Sub_metering_2)
-pwrcons$Sub_metering_3 <- as.double(pwrcons$Sub_metering_3)
+# read the data in
+pwrcons2 <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?")
 # subset for two days
-dates <- as.Date(c("2007-02-01", "2007-02-02"))
-str(dates)
-pwrsub <- subset(pwrcons, Date == dates[1] | Date == dates[2])
+pwrsub2 <- subset(pwrcons2, pwrcons2$Date = "1/2/2007" | pwrcons2$Date = "2/2/2007")
+# form a datetime column
+pwrsub2$DtTm <- strptime(paste(pwrsub2$Date, pwrsub2$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
 # save data
-write.table(pwrsub, "subset.txt", sep=";")
-library(xlsx)
-write.xlsx(pwrsub, "subset.xlsx")
+write.table(pwrsub2, "subset.txt", sep=";")
